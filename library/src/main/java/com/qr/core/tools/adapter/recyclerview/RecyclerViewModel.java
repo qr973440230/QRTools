2b7d8ab130a00439dc1d55f1a777a4612fc55560
锐气波发 <973440230@qq.com>
2019-04-22 14:39:40 +0800

    删除数据内容

--- library/src/main/java/com/qr/core/tools/adapter/recyclerview/RecyclerViewModel.java
+++ library/src/main/java/com/qr/core/tools/adapter/recyclerview/RecyclerViewModel.java
@@ -1,18 +1,9 @@
 package com.qr.core.tools.adapter.recyclerview;
 
-
 import java.util.List;
 
-public abstract class RecyclerViewModel<T> {
-    private final T data;
-    public RecyclerViewModel(T data) {
-        this.data = data;
-    }
-
+public abstract class RecyclerViewModel{
     public abstract int spinSize();
     public abstract int layoutId();
     public abstract void bindView(RecyclerViewViewHolder holder, int position, List<Object> payloads);
-    public T getData() {
-        return data;
-    }
 }
