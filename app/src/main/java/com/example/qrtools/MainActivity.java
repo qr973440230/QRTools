c5ddd3d7cf3b774db2f87ad9ddfa13fc3fb9a721
锐气波发 <973440230@qq.com>
2019-06-05 09:31:37 +0800

    删除PopupView

--- app/src/main/java/com/example/qrtools/MainActivity.java
+++ app/src/main/java/com/example/qrtools/MainActivity.java
@@ -8,13 +8,29 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import com.qr.core.view.popup.impl.SpinKitLoading;
+
 public class MainActivity extends AppCompatActivity {
     public static String TAG = MainActivity.class.getName();
+    SpinKitLoading spinKitLoading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(spinKitLoading == null){
+                    spinKitLoading = new SpinKitLoading(MainActivity.this)
+                            .setTitle("正在加载中...");
+                    spinKitLoading.show();
+                }else{
+                    spinKitLoading.dismiss();
+                    spinKitLoading = null;
+                }
+            }
+        });
     }
 
     @Override
